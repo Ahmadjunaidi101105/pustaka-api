8dd8c18a4f9aa6d0552698bcf40092602058fa57
“Ahmad <ahmadjunaidibeds@gmail.com>
2025-06-10 23:59:59 +0700

    refactor: remove path argument from LoadConfig function and update calls

--- utils/config.go
+++ utils/config.go
@@ -7,13 +7,15 @@ import (
 // Config contains the configuration for the application.
 // the values are read by viper from a config file or environment variables.
 type Config struct {
-	DSN               string `mapstructure:"DSN"`
+	DSN                 string `mapstructure:"DSN"`
 	HTTPServerAddress string `mapstructure:"HTTP_SERVER_ADDRESS"`
 }
 
 // LoadConfig loads the configuration from a config file or environment variables.
-func LoadConfig(path string) (config Config, err error) {
-	viper.AddConfigPath(path)
+// func LoadConfig(path string) (config Config, err error) { // <--- HAPUS PATH DARI ARGUMEN
+func LoadConfig() (config Config, err error) { // <--- JADI SEPERTI INI
+	// viper.AddConfigPath(path) // <--- HAPUS BARIS INI
+	viper.AddConfigPath(".") // <--- Tambahkan ini untuk selalu mencari di direktori saat ini
 	viper.SetConfigName("app")
 	viper.SetConfigType("env")
 
